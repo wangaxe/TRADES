adf4c5bc53d9fb3d23106bd6b8669902ff2b86f7
unknown <13633827530@163.com>
2020-11-09 13:42:56 +0800

    1109

--- train_trades_cifar10.py
+++ train_trades_cifar10.py
@@ -66,9 +66,9 @@ transform_train = transforms.Compose([
 transform_test = transforms.Compose([
     transforms.ToTensor(),
 ])
-trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='/mnt/storage0_8/torch_datasets/cifar-data', train=True, download=True, transform=transform_train)
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, **kwargs)
-testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='/mnt/storage0_8/torch_datasets/cifar-data', train=False, download=True, transform=transform_test)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
 
